01795b1afb54e99e055326ee2750bab0d62d48f0
AVenturi98 <lssndrventuri@gmail.com>
2025-05-04 23:28:44 +0200

    snack 3

--- snacks-func.js
+++ snacks-func.js
@@ -4,10 +4,26 @@ function getInitials(name) {
     return `${nome.at().toUpperCase()}${cognome.at().toUpperCase()}`
 }
 
-
 // SNACK 2
 function createSlug(name) {
     return name.toLowerCase()
 }
 
-module.exports = { getInitials, createSlug }
+// SNACK 3
+function average(arr) {
+
+    arr.forEach((n) => {
+        if (isNaN(n)) {
+            throw new Error('Accetta solo numeri')
+        }
+    })
+
+    const sum = arr.reduce((acc, num) => acc + num, 0);
+    return sum / arr.length
+}
+
+module.exports = {
+    getInitials,
+    createSlug,
+    average
+}
--- snacks-test.test.js
+++ snacks-test.test.js
@@ -1,16 +1,24 @@
 const {
     getInitials,
-    createSlug
+    createSlug,
+    average
 } = require('./snacks-func');
 
 
 // SNACK 1
-test("La funzione getInitials restituisce le iniziali di un nome completo.", () => {
+test("La funzione 'getInitials' restituisce le iniziali di un nome completo.", () => {
     expect(getInitials('John Doe')).toBe('JD');
 })
 
 
 // SNACK 2
-test("La funzione createSlug restituisce una stringa in lowercase.", () => {
+test("La funzione 'createSlug' restituisce una stringa in lowercase.", () => {
     expect(createSlug('JOHN DOE')).toBe('john doe');
+})
+
+
+// SNACK 3
+test("La funzione 'average' calcola la media aritmetica di un array di numeri.", () => {
+    expect(average([1, 2, 3, 4, 5])).toBe(3);
+    expect(() => average([1, ' numero'])).toThrow();
 })
